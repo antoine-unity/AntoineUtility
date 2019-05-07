54bac5b73559f977ee812f3db707d20adc77cd57
antoine-unity <antoinebr@unity3d.com>
2019-05-07 17:31:11 -0400

    Added force recompile

---
+++ Editor/ForceRecompile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+namespace AntoineUtility
+{
+    internal static class ForceRecompile
+    {
+        [MenuItem("Utility/Recompile"), Shortcut("Recompile", KeyCode.K, ShortcutModifiers.Action)]
+        static void TriggerForceRecompile()
+        {
+            var targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            var previous = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetGroup);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(targetGroup, "Test");
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(targetGroup, previous);
+        }
+    }
+}
